ad6a99461e36d81734d76013028e600203c371b7
Shital <shitalbk36@gmail.com>
2021-09-23 23:59:11 -0400

    Added new assignemnt

---
+++ dom-assignment-start/assignment.js
@@ -0,0 +1,23 @@
+//Task One
+//First Style
+// const listOne = document.getElementById('task-1');
+// listOne.style.backgroundColor = 'black';
+// listOne.style.color = 'white';
+
+//Second Style 
+const listTwo = document.querySelector('#task-1');
+listTwo.style.backgroundColor = 'green';
+listTwo.style.color = 'white';
+
+// Task Two
+//First Style
+const docTitle = document.querySelector('#title');
+docTitle.textContent = "Assignment-Solved!!";
+
+//Second Two
+// const docTitleOne = document.querySelector('#title');
+// docTitleOne
+
+//Task Three
+const headingOne = document.getElementsByTagName('h1');
+headingOne.textContent = "Assignment - Solved!!";
