a2de966ed935f3c8de87952cadf63e6772bc4566
Shital <shitalbk36@gmail.com>
2021-10-24 15:42:13 -0400

    Class addition

--- oop-01-starting-setup-2/assets/scripts/app.js
+++ oop-01-starting-setup-2/assets/scripts/app.js
@@ -1,23 +1,38 @@
+// Product Class Creation
+class Product{
+  title = 'DEFAULT';
+  imageUrl;
+  price;
+  description;
+
+  constructor(title,image,price,desc){
+    this.title = title;
+    this.imageUrl = image;
+    this.price = price;
+    this.description = desc;
+  }
+}
+
 const productList = {
    products: [
-    {
-      title: 'A Pillow',
-      imageUrl: 'https://i5.walmartimages.com/asr/4ac85746-c5ed-4b39-96f6-db9e4c85a62e_1.2f506af536237b234b7d0008bca6e43c.jpeg',
-      price: 18.89,
-      description: 'A soft pillow'
-    },
-    {
-      title: 'A Headset',
-      imageUrl: 'https://exooto.com/wp-content/uploads/2018/09/Lightweight-Aluminum-Headband-Retractable-Mic-in-Line-Remote-Gaming-Headset-Pewdiepie-Edition_1.jpg',
-      price: 79.99,
-      description: 'A Gaming Headset'
-    },
-    {
-      title: 'A Cup',
-      imageUrl: 'https://www.zastavki.com/pictures/1920x1200/2011/Food_Drinks_Cup_of_fresh_coffee_028669_.jpg',
-      price: 1.89,
-      description: 'A Coffee Cup'
-    }
+     new Product(
+      'A Pillow',
+      'https://i5.walmartimages.com/asr/4ac85746-c5ed-4b39-96f6-db9e4c85a62e_1.2f506af536237b234b7d0008bca6e43c.jpeg',
+      18.89,
+      'A soft pillow'
+     ),
+    new Product(
+      'A Headset',
+      'https://exooto.com/wp-content/uploads/2018/09/Lightweight-Aluminum-Headband-Retractable-Mic-in-Line-Remote-Gaming-Headset-Pewdiepie-Edition_1.jpg',
+      79.99,
+      'A Gaming Headset'
+    ),
+    new Product(
+      'A Cup',
+      'https://www.zastavki.com/pictures/1920x1200/2011/Food_Drinks_Cup_of_fresh_coffee_028669_.jpg',
+      1.89,
+      'A Coffee Cup'
+    )
   ],
   render(){
     const renderHook = document.getElementById('app');
