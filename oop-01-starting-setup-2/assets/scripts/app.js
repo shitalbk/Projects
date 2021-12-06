4143907f4a065bb1f2b6089d6145f5a1567fbe11
Shital <shitalbk36@gmail.com>
2021-12-05 23:07:07 -0500

    Adding Inherited classes

--- oop-01-starting-setup-2/assets/scripts/app.js
+++ oop-01-starting-setup-2/assets/scripts/app.js
@@ -1,11 +1,11 @@
 // Product Class Creation
-class Product{
+class Product {
   // title = 'DEFAULT';
   // imageUrl;
   // price;
   // description;
 
-  constructor(title,image,price,desc){
+  constructor(title, image, price, desc) {
     this.title = title;
     this.imageUrl = image;
     this.price = price;
@@ -13,38 +13,80 @@ class Product{
   }
 }
 
-class ShoppingCart{
+class ElementAttribute {
+  constructor(attrName, attrValue) {
+    this.name = attrName;
+    this.value = attrValue;
+  }
+}
+
+class Component {
+  constructor(renderHookId) {
+    this.hookId = renderHookId;
+  }
+
+  createRootElement(tag, cssClasses, attributes) {
+    const rootElement = document.createElement(tag);
+    if (cssClasses) {
+      rootElement.className = cssClasses;
+    }
+    if (attributes && attributes.length > 0) {
+      for (const attr of attributes) {
+        rootElement.setAttribute(attr.name, attr.value);
+      }
+    }
+    document.getElementById(this.hookId).append(rootElement);
+    return rootElement;
+  }
+}
+
+class ShoppingCart extends Component {
   items = [];
 
-  addProduct(product){
-    this.items.push(product);
-    this.totalOutput = `<h2>Total: \$${1}</h2>`;
-    
+  set cartItems(value) {
+    this.items = value;
+    this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(
+      2
+    )}</h2>`;
   }
-  render(){
-    const cartEl = document.createElement('section');
+
+  get totalAmount() {
+    const sum = this.items.reduce(
+      (prevValue, curItem) => prevValue + curItem.price,
+      0
+    );
+    return sum;
+  }
+
+  constructor(renderHookId){
+    super(renderHookId);
+  }
+
+  addProduct(product) {
+    const updatedItems = [...this.items];
+    updatedItems.push(product);
+    this.cartItems = updatedItems;
+  }
+  render() {
+    const cartEl = this.createRootElement('section','cart');
     cartEl.innerHTML = `
     <h2>Total: \$${0}</h2>
     <button>Order Now!!</button>
     `;
-    cartEl.className = 'cart';
-    this.totalOutput = cartEl.querySelector('h2');
-
-    return cartEl;
+    this.totalOutput = cartEl.querySelector("h2");
   }
 }
 
-class ProductItem{
-  constructor(product){
+class ProductItem {
+  constructor(product) {
     this.product = product;
   }
-  addToCart(){
-    console.log('Adding product to cart...');
-    console.log(this.product);
+  addToCart() {
+    App.addProductToCart(this.product);
   }
-  render(){
-    const prodEl = document.createElement('li');
-    prodEl.className = 'product-item';
+  render() {
+    const prodEl = document.createElement("li");
+    prodEl.className = "product-item";
     prodEl.innerHTML = `
     <div>
       <img src="${this.product.imageUrl}" alt="${this.product.title}">
@@ -56,40 +98,39 @@ class ProductItem{
       </div>
     </div>
     `;
-    const addCartButton = prodEl.querySelector('button');
-    addCartButton.addEventListener('click',this.addToCart.bind(this));
+    const addCartButton = prodEl.querySelector("button");
+    addCartButton.addEventListener("click", this.addToCart.bind(this));
     return prodEl;
   }
 }
 
-class ProductList{
+class ProductList {
   products = [
     new Product(
-      'A Pillow',
-      'https://i5.walmartimages.com/asr/4ac85746-c5ed-4b39-96f6-db9e4c85a62e_1.2f506af536237b234b7d0008bca6e43c.jpeg',
+      "A Pillow",
+      "https://i5.walmartimages.com/asr/4ac85746-c5ed-4b39-96f6-db9e4c85a62e_1.2f506af536237b234b7d0008bca6e43c.jpeg",
       18.89,
-      'A soft pillow'
-     ),
-    new Product(
-      'A Headset',
-      'https://exooto.com/wp-content/uploads/2018/09/Lightweight-Aluminum-Headband-Retractable-Mic-in-Line-Remote-Gaming-Headset-Pewdiepie-Edition_1.jpg',
-      79.99,
-      'A Gaming Headset'
+      "A soft pillow"
     ),
     new Product(
-      'A Cup',
-      'https://www.zastavki.com/pictures/1920x1200/2011/Food_Drinks_Cup_of_fresh_coffee_028669_.jpg',
+      "A Headset",
+      "https://exooto.com/wp-content/uploads/2018/09/Lightweight-Aluminum-Headband-Retractable-Mic-in-Line-Remote-Gaming-Headset-Pewdiepie-Edition_1.jpg",
+      79.99,
+      "A Gaming Headset"
+    ),
+    new Product(
+      "A Cup",
+      "https://www.zastavki.com/pictures/1920x1200/2011/Food_Drinks_Cup_of_fresh_coffee_028669_.jpg",
       1.89,
-      'A Coffee Cup'
+      "A Coffee Cup"
     )
   ];
 
-  constructor(){}
-  render(){
-    
-    const prodList = document.createElement('ul');
-    prodList.className = 'product-list';
-    for(const prod of this.products){
+  constructor() {}
+  render() {
+    const prodList = document.createElement("ul");
+    prodList.className = "product-list";
+    for (const prod of this.products) {
       const productItem = new ProductItem(prod);
       const prodEL = productItem.render();
       prodList.append(prodEL);
@@ -98,17 +139,28 @@ class ProductList{
   }
 }
 
-class Shop{
-  render(){
-    const renderHook = document.getElementById('app');
-    const cart = new ShoppingCart();
-    const cartEl = cart.render();
+class Shop {
+  render() {
+    const renderHook = document.getElementById("app");
+    this.cart = new ShoppingCart('app');
+    this.cart.render();
     const productList = new ProductList();
     const prodListEl = productList.render();
-    renderHook.append(cartEl);
     renderHook.append(prodListEl);
   }
 }
 
-const shop = new Shop();
-shop.render();
+class App {
+  static cart;
+
+  static init() {
+    const shop = new Shop();
+    shop.render();
+    this.cart = shop.cart;
+  }
+  static addProductToCart(product) {
+    this.cart.addProduct(product);
+  }
+}
+
+App.init();
