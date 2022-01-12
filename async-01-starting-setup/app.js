4c15f6980c7e12097fe8553b9cc9dc3dbbd1275e
Shital <shitalbk36@gmail.com>
2022-01-11 22:53:14 -0500

    Add folder

--- async-01-starting-setup/app.js
+++ async-01-starting-setup/app.js
@@ -3,10 +3,13 @@ const output = document.querySelector("p");
 
 const getPosition = opts => {
   const promise = new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(success => {
+    navigator.geolocation.getCurrentPosition(
+      success => {
         resolve(success);
       },
-      error => {},
+      error => {
+        reject(error);
+      },
       opts
     );
   });
@@ -22,23 +25,47 @@ const setTimer = duration => {
   return promise;
 };
 
-function trackUserHandler() {
-  let positionData;
-  getPosition()
-  .then(posData => {
-    positionData = posData;
-    return setTimer(2000);
-  })
-  .then(data => {
-    console.log(data, positionData);
-  });
-  setTimer(1000).then(() => {
-    console.log("Timer done!");
-  });
-  console.log("Getting position...");
+async function trackUserHandler() {
+  // let positionData;
+  let posData;
+  let timerData;
+  try {
+    posData = await getPosition();
+    timerData = await setTimer(2000);
+  } catch (error) {
+    console.log(error);
+  }
+  console.log(timerData, posData);
+  // .then(posData => {
+  //   positionData = posData;
+  //   return setTimer(2000);
+  // })
+  // .catch(err => {
+  //   console.log(err);
+  //   return 'on we go...';
+  // })
+  // .then(data => {
+  //   console.log(data, positionData);kljkljk
+  // });
+  // setTimer(1000).then(() => {
+  //   console.log("Timer done!");
+  // });
+  // console.log("Getting position...");
 }
 
 button.addEventListener("click", trackUserHandler);
+
+// Promise.race([getPosition(), setTimer(1000)]).then(data => {
+//   console.log(data);
+// });
+
+// Promise.all([getPosition(), setTimer(1000)]).then(promiseData => {
+//   console.log(promiseData);
+// });
+
+Promise.allSettled([getPosition(), setTimer(1000)]).then(promiseData => {
+  console.log(promiseData);
+});
 
 // let result = 0;
 // for(let i =0;i<10000000;i++){
